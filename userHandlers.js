83cb740c79107c8f88e567c92611416a00e6a1ca
sdp971 <doris190485@hotmail.com>
2023-03-19 17:46:30 +0100

    done quest 5

--- userHandlers.js
+++ userHandlers.js
@@ -59,7 +59,7 @@ const updateUser = (req, res) => {
       if (result.affectedRows === 0) {
         res.status(404).send('Not Found');
       } else {
-        res.status(204);
+        res.sendStatus(204);
       }
     })
     .catch((err) => {
@@ -68,6 +68,23 @@ const updateUser = (req, res) => {
     });
 };
 
+const deleteUser = (req, res) => {
+  const id = parseInt(req.params.id);
+  database
+    .query('delete from users where id = ?', [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send('Not Found');
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error deleting the user');
+     })
+ }
+
 
 
 module.exports = {
@@ -75,4 +92,5 @@ module.exports = {
   getUsersById,
   postUser,
   updateUser,
+  deleteUser,
 };
