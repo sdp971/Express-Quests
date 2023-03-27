3cc3b71c858797e7edc3ed51242bc5d9c81b330d
sdp971 <doris190485@hotmail.com>
2023-03-27 09:41:44 +0200

    done quest express04bis

--- app.js
+++ app.js
@@ -12,22 +12,29 @@ const welcome = (req, res) => {
 };
 
 
+
+
+
 app.get('/', welcome);
 
 const movieHandlers = require('./movieHandlers');
+const { validateMovie } = require('./validators');
+
 
 app.get('/api/movies', movieHandlers.getMovies);
 app.get('/api/movies/:id', movieHandlers.getMovieById);
-app.post('/api/movies', movieHandlers.postMovie);
-app.put('/api/movies/:id', movieHandlers.updateMovie);
+app.post('/api/movies',validateMovie, movieHandlers.postMovie);
+app.put('/api/movies/:id',validateMovie, movieHandlers.updateMovie);
 app.delete('/api/movies/:id', movieHandlers.deleteMovie);
 
+
 const userHandlers = require('./userHandlers');
+const { validateUser } = require('./validators');
 
 app.get('/api/users', userHandlers.getUsers);
 app.get('/api/users/:id', userHandlers.getUsersById);
-app.post('/api/users', userHandlers.postUser);
-app.put('/api/users/:id', userHandlers.updateUser);
+app.post('/api/users',validateUser, userHandlers.postUser);
+app.put('/api/users/:id',validateUser, userHandlers.updateUser);
 app.delete('/api/users/:id', userHandlers.deleteUser);
 
 app.listen(port, (err) => {
